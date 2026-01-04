7a4bc3fe90fa89e4dff528161159ff9fee835eaf
Abd2lhakam19 <midoahme93@gmail.com>
2026-01-04 22:35:47 +0200

    Force HTML Renderer

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1815222349"
+    serviceWorkerVersion: "2061974221"
   }
 });
